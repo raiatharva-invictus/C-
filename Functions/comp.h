5aa9b1b797395f3a429ee129a3d613e97026ee71
Atharva Rai <raiatharva726@gmail.com>
2025-09-26 07:04:47 +0530

    Little bit of morning learning

---
+++ Functions/comp.h
@@ -0,0 +1,5 @@
+int max(int a, int b);
+int min(int a, int b);
+int sum(int a, int b);
+
+// This header file contains declaration to the functions defnitions can be present in any file in the project folder ad linker is going to find it and stitch it to our main file which we are going to compile
