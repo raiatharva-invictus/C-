c4724470ecc6f28cec288e6c22c44f4406d0c05c
Atharva Rai <raiatharva726@gmail.com>
2025-10-01 19:39:56 +0530

    learned things about fuction templates

--- Functions/ops.cpp
+++ Functions/ops.cpp
@@ -1,4 +1,4 @@
-int inc_mult(int a, int b){
-    return ((a++) * (++b)); // to multiply a first then increment it while b is incrementing first then multiplying we know this all things from prefix and postfic incrementation
-    // return ((++a) * (++b));// to increment then multiply
-}
+// int inc_mult(int a, int b){
+//     return ((a++) * (++b)); // to multiply a first then increment it while b is incrementing first then multiplying we know this all things from prefix and postfic incrementation
+//     // return ((++a) * (++b));// to increment then multiply
+// }
