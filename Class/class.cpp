0ec412d6f82eb0320e6a1a2520e1e1aa5de18be0
Atharva Rai <raiatharva726@gmail.com>
2025-10-13 12:31:15 +0530

    Started classes

--- Class/class.cpp
+++ Class/class.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
-
+using namespace std;
+#define PI 3.14159
+class Cylinder {
+    public:
+        double base_radius{2.4};
+        double height{5.0};
+    public :
+        double volume(){
+            return PI * base_radius  * base_radius * height ;
+        }
+};
 int main()
 {
-    
-}
+    Cylinder cylinder;
+    cout <<"Vol of Cylinder having " << "Base_Radius : " << cylinder.base_radius << " and Height : " << cylinder.height << " is " <<  cylinder.volume() << endl;
+}   
